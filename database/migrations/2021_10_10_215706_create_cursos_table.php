0d1ce00937aaee8e6a6ac51c3fbe473a8e3b3553
Jesus Ale <jesus.9314@gmail.com>
2021-10-10 17:08:52 -0500

    creando migraciones

--- database/migrations/2021_10_10_215706_create_cursos_table.php
+++ database/migrations/2021_10_10_215706_create_cursos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Cursos extends Migration
+class CreateCursosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,9 @@ class Cursos extends Migration
     public function up()
     {
         Schema::create('cursos', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            
+            $table->id();
+            $table->string('name');
+            $table->text('descripcion');
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ class Cursos extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('cursos');
     }
 }
