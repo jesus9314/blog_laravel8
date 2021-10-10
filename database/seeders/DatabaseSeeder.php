309e1f24a723f1927125b73d817f4d8f22e852e8
Jesus Ale <jesus.9314@gmail.com>
2021-10-10 18:32:38 -0500

    Creando Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Curso;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,8 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(CursoSeeder::class);
-        
+        User::factory(10)->create();
+        //$this->call(CursoSeeder::class);
+        Curso::factory(50)->create();
     }
 }
